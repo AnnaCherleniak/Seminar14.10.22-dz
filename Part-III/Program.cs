f96e742e2426be9eadb9c887de8909ab7bc4802c
Anna <ankakras@yandex.ru>
2022-10-21 19:06:48 +0700

    Решение

--- Part-III/Program.cs
+++ Part-III/Program.cs
@@ -145,6 +145,107 @@ bool SortedArray(int[]array)
     return true;
 }
 
+//II. Реализовать след функции:
+// 1. Функцию, которая вычисляет число a в степени n
+int PowerNumber(int a, int n)
+{
+    int result = 1;
+    while(n > 0)
+    {
+        result = result * a;
+        n--;
+    }
+    return result;
+}
+// 2. Функцию, которая вычисляет факториал числа n
+int FactorialNumber(int n)
+{
+    int result = 1;
+    for(int i = 1; i <= n; i++)
+        result = result * i;
+    return result;
+}
+// 3. Функцию, которая вычисляет сумму цифр произвольного целого числа n
+int SummDigits(int n)
+{
+    int temp = 0;
+    int sum = 0;
+    while(n > 0)
+    {
+        temp = n % 10;
+        sum = sum + temp;
+        n = n/10;
+    }
+    return sum;
+}
+// 4. Функцию, которая проверяет является ли заданное число n полиндромом
+void Palindrome( int n)
+{
+    int N = n;
+    int length = 0;
+    while(N > 0)
+    {
+        N/=10;
+        length++;
+    }
+    int[] digits = new int[length];
+    int i = length - 1;
+    while(n > 0)
+    {
+        digits[i] = n % 10;
+        n = n/10;
+        i = i-1;
+    }
+    i = 0;
+    int j = length - 1;
+    int count = 0;
+    while(i < j)
+    {
+        if(digits[i]!= digits[length - i - 1])
+            count++;
+        i++;
+        j--;
+    }
+    if(count > 0)
+        Console.WriteLine("Число Не является палиндромом");
+    else
+    {
+        Console.WriteLine("Число является Палиндромом");
+    }
+}
+
+// 5. Функцию, складывающую два целых числа
+int SummNumbers(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
+// 6. Функцию, определяющую является ли число простым, 
+//то есть возвращающую true, если число простое, иначе - false
+bool PrimeNumber(int n)
+{
+    if(n > 1)
+    {
+        for(int x = 2; x < n; x++)
+            if(n % x != 0)
+                return true;
+            else
+                return false;
+    }
+    return true;
+}
+// 7. Функцию, определяющую является ли число чётным, 
+//то есть возвращающую true, если число чётное, иначе - false
+
+bool Check(int n)
+{
+    if(n % 2 == 0)
+        return true;
+    else
+        return false;
+    return true;
+}
 
 
 Console.WriteLine("Введите длину массива - ");
@@ -186,3 +287,34 @@ Console.WriteLine($"Количество положительных элемен
 Console.WriteLine($"Количество нечетных элементов массива - {CountOddNumbers(array)}");
 
 Console.WriteLine($"Отсортирован ли наш массив - {SortedArray(array)}");
+
+Console.WriteLine("Вторая часть задания");
+
+Console.WriteLine("Введите число (a) - ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите степень (n), в которую возвести число - ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Число *a* в степени *n* = {PowerNumber(a, n)}");
+
+Console.WriteLine("Введите число - ");
+n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Факториал числа = {FactorialNumber(n)}");
+
+Console.WriteLine($"Сумма цифр числа = {SummDigits(n)}");
+
+Console.WriteLine("Введите число, которое проверить на палиндром - ");
+n = int.Parse(Console.ReadLine() ?? "0");
+Palindrome(n);
+
+Console.WriteLine("Введите первое число - ");
+a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число - ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Сумма двух чисел = {SummNumbers(a, b)}");
+
+Console.WriteLine("Введите число - ");
+n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Является ли число простым - {PrimeNumber(n)}");
+
+
+Console.WriteLine($"Является ли число четным - {Check(n)}");
